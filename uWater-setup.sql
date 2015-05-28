a4a7674dd7f650e2e1f9de9eb5b80fd5297080d9
Sam Wu <sjiwu14@gmail.com>
2015-05-28 23:08:23 +0000

    initial commit of db schema

--- uWater-setup.sql
+++ uWater-setup.sql
@@ -8,7 +8,8 @@ create table Users(
 
 create table UserSetting(
   userId int primary key,
-  minMoisture float DEFAULT 0.1,
+  minMoisture float DEFAULT 0.01,
+  timeOfDayToWater time DEFAULT '06:00:00',
   minPrecipitationChance float DEFAULT 0.25,
   maxTimeToRain int DEFAULT 480,
   lat float,
@@ -17,11 +18,19 @@ create table UserSetting(
 );
 
 create table DeviceData(
-  userId int primary key,
+  id int primary key auto_increment,
+  userId int NOT NULL,
   timestamp datetime,
   humidity float,
   relay int,
   CONSTRAINT DDU_FK FOREIGN KEY (userId) REFERENCES Users(id)
 );
 
+create table WeatherData(
+  id int primary key auto_increment,
+  userId int NOT NULL,
+  timestamp datetime,
+  precipProbability float,
+  CONSTRAINT WDU_FK FOREIGN KEY (userId) REFERENCES Users(id)
+);
 
